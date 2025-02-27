c7693fe67026b57ea4b4faf16cbb2fd8564a9852
rzeczuchy <jagielnicki.michal@gmail.com>
2025-02-27 22:00:20 +0100

    make window frameless and finish styling

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    resizable: false,
+    frame: false,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
