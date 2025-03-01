690092e77981fc72c1301fd7fb720d2cf7feb7c6
rzeczuchy <jagielnicki.michal@gmail.com>
2025-03-01 06:50:38 +0100

    adding button controls and file path input

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Text Editor Electron
 
 A simple desktop text editor app with Node.js + Electron, scaffolded with Electron Forge.
-
-Focused text editor with almost no GUI, only text and keyboard shortcuts.
--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,23 @@
 const textArea = document.getElementById("mainTextArea");
 
 textArea.focus();
-textArea.value = 'open file => ctrl + o\nsave file => ctrl + s\nexit => alt + f4';
+textArea.value = "";
+
+textArea.addEventListener("drop", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  console.log(`File <${e.dataTransfer.files[0].name}> dropped in drop space.`);
+});
+
+textArea.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+});
+
+textArea.addEventListener("dragenter", (e) => {
+  console.log("File is in the Drop Space");
+});
+
+textArea.addEventListener("dragleave", (e) => {
+  console.log("File has left the Drop Space");
+});
