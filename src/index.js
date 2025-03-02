849e1168344798a2dfdd715b470d5cec26c2da27
rzeczuchy <jagielnicki.michal@gmail.com>
2025-03-02 17:24:13 +0100

    saving files

--- src/index.js
+++ src/index.js
@@ -1,28 +1,73 @@
 const { app, dialog, Menu, BrowserWindow, webContents } = require("electron");
-const { readFile } = require("node:fs");
+const { readFile, writeFileSync } = require("node:fs");
 const path = require("node:path");
 
 if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
-const newFile = (mainWindow) => {};
+let currentFilePath = null;
 
-const openFile = async (mainWindow) => {
-  const { canceled, filePaths } = await dialog.showOpenDialog({});
-  if (!canceled) {
-    const path = filePaths[0];
-    readFile(path, "utf8", (err, data) => {
-      if (err) throw err;
-      mainWindow.webContents.send("open-file", data);
-      mainWindow.setTitle(path);
-    });
+const setCurrentFilePath = (window, path) => {
+  console.log(path);
+  currentFilePath = path;
+  // TODO: properly validate path
+  if (path == "") {
+    window.setTitle("Untitled");
+  } else {
+    window.setTitle(path);
   }
 };
 
-const saveFile = (mainWindow) => {};
+const newFile = (mainWindow) => {
+  setCurrentFilePath(mainWindow, "");
+};
 
-const saveFileAs = (mainWindow) => {};
+const openFile = async (mainWindow) => {
+  dialog
+    .showOpenDialog({})
+    .then((result) => {
+      if (!result.canceled) {
+        const path = result.filePaths[0];
+        readFile(path, "utf8", (err, data) => {
+          if (err) throw err;
+          mainWindow.webContents.send("open-file", data);
+          setCurrentFilePath(mainWindow, path);
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const showSaveFileDialog = (mainWindow) => {
+  dialog
+    .showSaveDialog(mainWindow, {})
+    .then((result) => {
+      if (!result.canceled) {
+        const path = result.filePath;
+        writeFileSync(path, "testing save 123");
+        setCurrentFilePath(mainWindow, path);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const saveFile = (mainWindow) => {
+  // TODO: properly validate file path
+  if (currentFilePath === "") {
+    showSaveFileDialog(mainWindow);
+  } else {
+    writeFileSync(currentFilePath, "testing save 123");
+  }
+};
+
+const saveFileAs = async (mainWindow) => {
+  showSaveFileDialog(mainWindow);
+};
 
 const createMenuTemplate = (mainWindow) => {
   const template = [
@@ -110,6 +155,7 @@ const createWindow = () => {
 
   const menu = createMenuTemplate(mainWindow);
   Menu.setApplicationMenu(menu);
+  setCurrentFilePath(mainWindow, "");
 
   mainWindow.loadFile(path.join(__dirname, "index.html"));
 };
