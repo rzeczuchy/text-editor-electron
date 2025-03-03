ac53a464ce8288310b3b7a03410e30e6ce7cedd7
rzeczuchy <jagielnicki.michal@gmail.com>
2025-03-03 06:49:40 +0100

    #8 prevent default on tab key

--- src/renderer.js
+++ src/renderer.js
@@ -3,6 +3,13 @@ const textArea = document.getElementById("mainTextArea");
 textArea.focus();
 textArea.value = "";
 
+window.onkeydown = (e) => {
+  if ((e.key == "Tab")) {
+    e.preventDefault();
+    textArea.value += "\t";
+  }
+};
+
 window.electronAPI.onOpenFile((value) => {
   textArea.value = value;
 });
